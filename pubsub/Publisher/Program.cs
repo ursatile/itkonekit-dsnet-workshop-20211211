948b56cbf87ae85546cbc94e103d09ab1a8037a7
dylanbeattie <dylan@dylanbeattie.net>
2021-12-11 14:01:59 +0000

    Subscriber ID and round-robindemo

--- pubsub/Publisher/Program.cs
+++ pubsub/Publisher/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using EasyNetQ;
 
-namespace Publisher
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Publisher {
+    class Program {
+        const string AMQP = "amqps://gsfihevq:eFtfoAM2d-JvSPxFBMu8te_VYbO91cBN@bunny.rmq.cloudamqp.com/gsfihevq";
+
+        static void Main(string[] args) {
+            using var bus = RabbitHutch.CreateBus(AMQP);
+            var count = 1;
+            while (true) {
+                Console.WriteLine("Press a key to publish a message...");
+                Console.ReadKey(false);
+                var message = $"Message {count++} from {Environment.MachineName} at {DateTime.UtcNow:O}";
+                bus.PubSub.Publish(message);
+                Console.WriteLine($"Published message: {message}");
+            }
         }
     }
 }
--- pubsub/Subscriber/Program.cs
+++ pubsub/Subscriber/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using EasyNetQ;
 
-namespace Subscriber
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Subscriber {
+    class Program {
+        const string AMQP = "amqps://gsfihevq:eFtfoAM2d-JvSPxFBMu8te_VYbO91cBN@bunny.rmq.cloudamqp.com/gsfihevq";
+
+        static void Main(string[] args) {
+            using var bus = RabbitHutch.CreateBus(AMQP);
+            var subscriberId = "itkonekt";
+            bus.PubSub.Subscribe<string>(subscriberId, message => Console.WriteLine(message));
+            Console.WriteLine("Subscribed! Listening for messages...");
+            Console.ReadLine();
         }
     }
 }
