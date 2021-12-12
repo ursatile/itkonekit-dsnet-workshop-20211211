9a09396f2bc46e941a93e81b3946b9fb3b29437f
dylanbeattie <dylan@dylanbeattie.net>
2021-12-12 14:09:12 +0000

    Working gRPC implementation, with servers in both ASP.NET and in Python

--- dotnet/Autobarn.PricingClient/Program.cs
+++ dotnet/Autobarn.PricingClient/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Autobarn.Messages;
+using Autobarn.PricingEngine;
+using EasyNetQ;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
 
-namespace Autobarn.PricingClient
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Autobarn.PricingClient {
+    class Program {
+        private const string SUBSCRIBER_ID = "Autobarn.PricingClient";
+        private static readonly IConfigurationRoot config = ReadConfiguration();
+        private static IBus bus;
+        private static Pricer.PricerClient grpcClient;
+
+        static async Task Main(string[] args) {
+            Console.WriteLine("Running Autobarn.PricingClient.");
+            bus = RabbitHutch.CreateBus(config.GetConnectionString("AutobarnRabbitMQConnectionString"));
+            using var channel = GrpcChannel.ForAddress(config["AutobarnPricingServerEndpoint"]);
+            grpcClient = new Pricer.PricerClient(channel);
+            Console.WriteLine("Connected to gRPC! Subscribing to messages...");
+            await bus.PubSub.SubscribeAsync<NewVehicleMessage>(SUBSCRIBER_ID, HandleNewVehicleMessage);
+            Console.WriteLine("Subscribed to NewVehicleMessages");
+            Console.ReadLine();
+        }
+
+        private static async Task HandleNewVehicleMessage(NewVehicleMessage arg) {
+            var request = new PriceRequest() {
+                ManufacturerName = arg.Manufacturer,
+                ModelName = arg.ModelName,
+                Color = arg.Color,
+                Year = arg.Year
+            };
+            var price = await grpcClient.GetPriceAsync(request);
+            Console.WriteLine(
+                $"Got a price: {arg.Manufacturer} {arg.ModelName} ({arg.Color}, {arg.Year}) : {price.Price} {price.CurrencyCode}");
+        }
+
+        private static IConfigurationRoot ReadConfiguration() {
+            var basePath = Directory.GetParent(AppContext.BaseDirectory).FullName;
+            return new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
         }
     }
 }
--- dotnet/Autobarn.PricingServer/Services/GreeterService.cs
+++ dotnet/Autobarn.PricingServer/Services/GreeterService.cs
@@ -1,26 +1,28 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Autobarn.PricingEngine;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 
-namespace Autobarn.PricingServer
-{
-    public class GreeterService : Greeter.GreeterBase
-    {
-        private readonly ILogger<GreeterService> _logger;
-        public GreeterService(ILogger<GreeterService> logger)
-        {
-            _logger = logger;
+namespace Autobarn.PricingServer.Services {
+    public class PricerService : Pricer.PricerBase {
+        private readonly ILogger<PricerService> logger;
+        public PricerService(ILogger<PricerService> logger) {
+            this.logger = logger;
         }
 
-        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-        {
-            return Task.FromResult(new HelloReply
-            {
-                Message = "Hello " + request.Name
+        public override Task<PriceReply> GetPrice(PriceRequest request, ServerCallContext context) {
+            var (price, currencyCode) = CalculatePrice(request);
+
+            return Task.FromResult(new PriceReply {
+                Price = price,
+                CurrencyCode = currencyCode
             });
+        }
+
+        public (int, string) CalculatePrice(PriceRequest priceRequest) {
+            if (priceRequest.ManufacturerName == "DMC") return (100000, "USD");
+            return priceRequest.Color.Equals("orange", StringComparison.InvariantCultureIgnoreCase) ? (500, "GBP") : (2000, "EUR");
         }
     }
 }
