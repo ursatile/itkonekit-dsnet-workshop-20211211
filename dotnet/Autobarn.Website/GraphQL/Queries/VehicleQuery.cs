18e751954815bc5a6a93a49b1150331e516c3e1d
dylanbeattie <dylan@dylanbeattie.net>
2021-12-12 09:44:12 +0000

    Vehicles query on graphQL endpoint

---
+++ dotnet/Autobarn.Website/GraphQL/Queries/VehicleQuery.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Autobarn.Data;
+using Autobarn.Data.Entities;
+using Autobarn.Website.GraphQL.GraphTypes;
+using GraphQL;
+using GraphQL.Language.AST;
+using GraphQL.Types;
+
+namespace Autobarn.Website.GraphQL.Queries {
+    public class VehicleQuery : ObjectGraphType {
+        private readonly IAutobarnDatabase db;
+
+        public VehicleQuery(IAutobarnDatabase db)
+        {
+            this.db = db;
+            Field<ListGraphType<VehicleGraphType>>("Vehicles",
+                "Query to retrieve all vehicles",
+                resolve: GetAllVehicles);
+        }
+
+        private IEnumerable<Vehicle> GetAllVehicles(IResolveFieldContext<object> context) => db.ListVehicles();
+
+    }
+}
